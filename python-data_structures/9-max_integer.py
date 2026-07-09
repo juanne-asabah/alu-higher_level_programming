10311cd55f318ca40414502ca5ddfaaba88d8057
juanne-asabah <j.asbah@alustudent.com>
2026-07-09 07:48:51 -0700

    4

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-        biggest = my_list[0]
+    biggest = my_list[0]
     for num in my_list:
         if num > biggest:
             biggest = num
