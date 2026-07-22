fe8472b8ef97801c28d9a010985bd54ad49a5ab6
juanne-asabah <j.asabah@alustudent.com>
2026-07-22 04:51:24 -0700

    1

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+class Rectangle:
+    """This is an empty class defining a rectangle."""
+    pass
---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+class Rectangle:
+    """This is a class defining a rectangle with private width and height attributes."""
+    def __init__(self, width=0, height=0):
+        """This initializes the rectangle with optional width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter: Retrieve the private width attributes"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter: Set the private width with type and value validatation rules"""
+        if not isinstance(value, int) or isinstance(value, bool):
+            raise TypeError("width must be an integer")
+        if value < 0:
+           raise ValueError("width must be >=0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter: Retrieve the private height attributes"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter: Set the private height with type and value validataion rules"""
+        if not isinstance(value, int) or isinstance(value, bool):
+            raise TypeError("width must be an integer")
+        if value <0:
+            rais ValueError("width must be >=0")
+        self.height = value
