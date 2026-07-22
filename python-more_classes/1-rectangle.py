fbd6db47916dc4ab81af2e880aa21c2944069c99
juanne-asabah <j.asabah@alustudent.com>
2026-07-22 05:47:06 -0700

    1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -20,9 +20,9 @@ class Rectangle:
         if not isinstance(value, int) or isinstance(value, bool):
             raise TypeError("width must be an integer")
         if value < 0:
-           raise ValueError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
-
+]
     @property
     def height(self):
         """Getter: Retrieve the private height attributes"""
