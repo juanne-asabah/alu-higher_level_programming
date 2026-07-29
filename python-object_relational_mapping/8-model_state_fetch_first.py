42974f851e247393460704859142f3f5742ca858
juanne-asabah <j.asabah@alustudent.com>
2026-07-29 04:08:54 -0700

    feat: complete task 8 fetch first state via sqlalchemy script

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Prints the first State object from the database hbtn_0e_6_usa.
+Takes 3 arguments: mysql username, mysql password, and database name.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # Setup database connection engine using command line arguments
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    # Create a configured "Session" class and instantiate it
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query only the first State object sorted by id
+    first_state = session.query(State).order_by(State.id.asc()).first()
+
+    # Display results exactly as shown in the task specifications
+    if first_state is not None:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
+
+    # Close session
+    session.close()
