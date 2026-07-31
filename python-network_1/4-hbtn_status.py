c4617a9c6c2933b8bb31a0707b381fb307f9c00e
juanne-asabah <j.asabah@alustudent.com>
2026-07-31 14:48:15 -0700

    Update Task 4 script using only requests package

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""Fetches a URL status provided as an argument using the requests package."""
-import sys
+"""Fetches a URL status using the requests package."""
 import requests
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-        response = requests.get(url)
-        text = response.text
-        print("Body response:")
-        print("\t- type: {}".format(type(text)))
-        print("\t- content: {}".format(text))
+    url = 'https://hbtn.io'
+    response = requests.get(url)
+    text = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
