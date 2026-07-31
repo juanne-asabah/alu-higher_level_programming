cb2410efb57a379eb34b0e122c0574da42652c55
juanne-asabah <j.asabah@alustudent.com>
2026-07-31 14:53:26 -0700

    Implement Task 0 status check with urllib using local sys check

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,6 +5,13 @@ import urllib.request
 
 if __name__ == "__main__":
     url = 'https://alu-intranet.hbtn.io/status'
+    try:
+        import sys
+        if len(sys.argv) > 1:
+            url = sys.argv[1]
+    except Exception:
+        pass
+
     with urllib.request.urlopen(url) as response:
         html = response.read()
         print("Body response:")
