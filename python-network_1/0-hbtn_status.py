2222ba4bfd301cfa826f872bdb7d15fb8932b9bf
juanne-asabah <j.asabah@alustudent.com>
2026-07-31 14:29:14 -0700

    Complete network tasks 0, 1, and 2 using urllib

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""
-This module fetches the status from a specific URL using urllib.
-"""
+"""Fetches a URL status using urllib."""
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://hbtn.io"
+    url = 'https://alu-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
