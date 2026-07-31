e0bbd8f2ffa27aee7fab46fb0b1dedc715b0c2f6
juanne-asabah <j.asabah@alustudent.com>
2026-07-31 14:22:53 -0700

    Fix missing argument handling boundary check for print_square function

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,7 +5,7 @@ composed of the '#' character based on a given size length.
 """
 
 
-def print_square(size):
+def print_square(size=None):
     """
     Prints a square with the character #.
 
@@ -16,9 +16,11 @@ def print_square(size):
         TypeError: If size is not an integer, or if it is a float less than 0.
         ValueError: If size is an integer less than 0.
     """
+    if size is None:
+        raise TypeError("size must be an integer")
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
-    if not isinstance(size, int):
+    if not isinstance(size, int) or type(size) is bool:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
