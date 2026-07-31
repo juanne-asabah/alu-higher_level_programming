13dca754322ff36996d58385dc3257a757d9db5d
juanne-asabah <j.asabah@alustudent.com>
2026-07-31 14:38:45 -0700

    Fix trailing whitespace on line 15

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -5,14 +5,15 @@ import requests
 
 
 if __name__ == "__main__":
-    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    q = sys.argv if len(sys.argv) > 1 else ""
     url = "http://0.0.0"
     payload = {'q': q}
 
     try:
         response = requests.post(url, data=payload)
         json_data = response.json()
-        if not json_data:
+
+        if json_data == {} or not json_data:
             print("No result")
         else:
             print("[{}] {}".format(json_data.get('id'), json_data.get('name')))
